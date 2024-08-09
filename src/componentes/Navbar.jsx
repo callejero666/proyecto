8605fdbe57beda311b610939c3aefd70ad3372a4
Rosana Mauno <rosanammauno@gmail.com>
2024-08-09 00:06:17 -0300

    Agregue carpeta hooks que contiene archivo useFetch.jsx

---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import './Navbar.css';
+
+export function Navbar() {
+    const { state, actions } = useAuth();
+
+    return (
+        <nav className="navbar">
+            <Link to="/">Inicio</Link>
+            {state.isAuthenticated ? (
+                <>
+                    <Link to="/Musica">Musica</Link>
+                    <Link to="/Chat">Chat</Link>
+                    <Link to="/perfil">Perfil</Link>
+                    
+                </>
+            ) : (
+                <>
+                    <Link to="/login">Iniciar Sesión</Link>
+                    <Link to="/registro">Registro</Link>
+                </>
+            )}
+        </nav>
+    );
+}
