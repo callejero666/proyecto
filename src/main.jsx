e3e95851bfc42fc8dd047266ca27dca380a371fd
callejero666 <rodrigocjsreyes666@gmail.com>
2024-08-09 01:28:19 -0300

    as

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
-import './index.css';
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
