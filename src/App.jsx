f5757b0902c7ccd93841bc76fdd0651b6bb09476
callejero666 <rodrigocjsreyes666@gmail.com>
2024-08-11 00:37:58 -0300

    login primero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, useNavigate } from 'react-router-dom';
 import { Chat } from './componentes/Chat';
 import { Login } from './componentes/Login';
 import { Musica } from './componentes/Musica';
@@ -12,20 +12,23 @@ import { AuthProvider, useAuth } from './contexts/AuthContext';
 
 function AppContent() {
     const { state, actions } = useAuth();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-        if (token && !state.isAuthenticated){
+        if (token && !state.isAuthenticated) {
             // Aquí podrías hacer una llamada a la API para validar el token y obtener los datos del usuario
             actions.login({ token });
+        } else if (!state.isAuthenticated) {
+            navigate('/login'); // Redirige a la página de inicio de sesión si no está autenticado
         }
-    }, [state.isAuthenticated, actions]);
+    }, [state.isAuthenticated, actions, navigate]);
 
     return (
         <>
             <Navbar />
             <Routes>
-                <Route path="/" element={<Principal />} />
+                <Route path="/" element={<Login />} /> {/* Cambiado para que Login sea la ruta principal */}
                 <Route path="/principal" element={<Principal />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/registro" element={<Registro />} />
